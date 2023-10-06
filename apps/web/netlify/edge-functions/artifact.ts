d071a06a9c673aff0129e091884c31cafed3c733
Matt Kane <m@mk.gg>
2023-10-06 22:22:32 +0100

    Make it work

--- apps/web/netlify/edge-functions/artifact.ts
+++ apps/web/netlify/edge-functions/artifact.ts
@@ -2,22 +2,40 @@ import type { Context as BlobContext } from "https://deploy-preview-243--edge.ne
 import type { Config, Context } from "https://edge.netlify.com/v1/index.ts";
 
 export const config: Config = {
-  method: ["GET", "PUT"],
-  path: "/v1/artifact/:id",
+  method: ["GET", "PUT", "HEAD"],
+  path: "/v8/artifacts/:hash",
 };
 
 export default async (request: Request, context: BlobContext & Context) => {
-  if(!config.method?.includes(request.method as any)) {
-    return new Response("Method not allowed", { status: 405 });
-  }
+  console.log(request.url);
+  console.log(request.headers, request.method);
+
   const bearerHeader = request.headers.get("authorization");
   const token = bearerHeader?.replace("Bearer ", "");
   if (!token || token !== Netlify.env.get("TURBO_TOKEN")) {
+    console.log("Unauthorized");
     return new Response("Unauthorized", { status: 401 });
   }
-  const { id } = context.params;
+  const { hash } = context.params;
 
-  console.log(context.blobs, token);
-  return new Response(`Artifact ${id}`);
+  if (request.method === "PUT") {
+    const blob = await request.arrayBuffer();
+    await context.blobs.put(hash, blob);
+    return new Response("OK");
+  }
+  try {
+    const blob = await context.blobs.get(hash, {
+      type: "arrayBuffer",
+    });
+    if (!blob) {
+      return new Response(`Artifact ${hash} not found`, { status: 404 });
+    }
+    const headers = new Headers();
+    headers.set("Content-Type", "application/octet-stream");
+    headers.set("Content-Length", blob.length.toString());
+    return new Response(blob, { headers });
+  } catch (e) {
+    console.log(e);
+    return new Response(e.message, { status: 500 });
+  }
 };
-
