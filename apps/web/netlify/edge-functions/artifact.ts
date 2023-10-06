be64c2dcce10f7edd4ceba5a0eddd763cd9c0fe0
Matt Kane <matt.kane@netlify.com>
2023-10-06 17:34:04 +0100

    More implementation

--- apps/web/netlify/edge-functions/artifact.ts
+++ apps/web/netlify/edge-functions/artifact.ts
@@ -1,12 +1,23 @@
-import type { Config, Context } from "@netlify/edge-functions";
-
-export default async (request: Request, context: Context) => {
-    const { id } = context.params;
-    console.log(context);
-    return new Response(`Artifact ${id}`)
-};
+import type { Context as BlobContext } from "https://deploy-preview-243--edge.netlify.app";
+import type { Config, Context } from "https://edge.netlify.com/v1/index.ts";
 
 export const config: Config = {
   method: ["GET", "PUT"],
-  path: "/artifact/:id",
+  path: "/v1/artifact/:id",
 };
+
+export default async (request: Request, context: BlobContext & Context) => {
+  if(!config.method?.includes(request.method as any)) {
+    return new Response("Method not allowed", { status: 405 });
+  }
+  const bearerHeader = request.headers.get("authorization");
+  const token = bearerHeader?.replace("Bearer ", "");
+  if (!token || token !== Netlify.env.get("TURBO_TOKEN")) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const { id } = context.params;
+
+  console.log(context.blobs, token);
+  return new Response(`Artifact ${id}`);
+};
+
